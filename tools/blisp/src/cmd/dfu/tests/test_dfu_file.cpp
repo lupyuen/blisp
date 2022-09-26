ea41715519ba3eb628df2cc2f5be4684a742e914
Ben V. Brown <Ralim@Ralimtek.com>
2022-09-26 22:20:05 +1000

    Test DFU file

--- tools/blisp/src/cmd/dfu/tests/test_dfu_file.cpp
+++ tools/blisp/src/cmd/dfu/tests/test_dfu_file.cpp
@@ -8,8 +8,10 @@
 TEST(DFU_FILE_PARSER, ParseTestFile) {
     uint8_t* payload = nullptr;
     size_t payload_size = 0;
-    int res = dfu_file_parse(SOURCE_DIR "/Pinecilv2_EN.dfu", &payload,
-                             &payload_size);
-    ASSERT_EQ(res, 0);
-    ASSERT_EQ(payload_size, 1024);
+    size_t payload_address = 0;
+    int res = dfu_file_parse(SOURCE_DIR "/test.dfu", &payload, &payload_size,
+                             &payload_address);
+    ASSERT_EQ(res, 1);
+    ASSERT_EQ(payload_size, 1337);
+    ASSERT_EQ(payload_address, 0x11223344);
 }
