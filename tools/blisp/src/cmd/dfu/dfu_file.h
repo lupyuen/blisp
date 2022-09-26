44e1da7b80fa9da535e88eefc5a106c34d6881d7
Ben V. Brown <Ralim@Ralimtek.com>
2022-09-26 21:00:25 +1000

    Create dfu_file.h

---
+++ tools/blisp/src/cmd/dfu/dfu_file.h
@@ -0,0 +1,25 @@
+//
+// Created by ralim on 26/09/22.
+//
+
+#ifndef BLISP_DFU_FILE_H
+#define BLISP_DFU_FILE_H
+
+
+#include <malloc.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int dfu_file_parse(const char* file_path_on_disk, uint8_t** payload,
+                   size_t* payload_length);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif // BLISP_DFU_FILE_H
